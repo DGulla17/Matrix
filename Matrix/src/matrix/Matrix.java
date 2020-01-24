3912649dc803f510516e8fc71ce71212baa5c7de
gulla.davide <gulla.davide@BIBLIO-PC07.OSDBSESTO.local>
2020-01-24 11:45:48 +0100

    add initial code

---
+++ Matrix/src/matrix/Matrix.java
@@ -0,0 +1,11 @@
+package matrix;
+
+public class Matrix {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("sososos");
+        
+    }
+    
+}
